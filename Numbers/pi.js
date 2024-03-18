9ed22bfcb502f696156780804a5ef060c682ec92
BruceICzw <h220396z@hit.ac.zw>
2024-03-18 15:13:09 +0200

    PI to the nth digit

--- Numbers/pi.js
+++ Numbers/pi.js
@@ -1,5 +1,8 @@
+//Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go
+
+
 const { log } = require('console');
-const readline = require('readline');
+const readline = require('readline');//for handling user input asynchronously in Node.js
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -13,6 +16,6 @@ rl.question('Enter the number of PI decimal places up to 10\n', (input) => {
         rl.close();
         return;
     }
-    log('Pi is ', Math.PI.toFixed(n), ' to ', n, ' decimal places');
+    log('Pi is ', Math.PI.toFixed(n), ` to ${n} decimal places`);
     rl.close();
 })
